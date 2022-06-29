cb268f4fe6a8dcd0909cdb51fd0da18f31c34439
captainazm <kouki.eastriv9@gmail.com>
2022-06-29 17:00:39 +0900

    + addition

--- sample.py
+++ sample.py
@@ -1,2 +1,5 @@
-num1 = input("first num is ")
-print(f"{num1}")
+num1 = int(input("first num is : "))
+num2 = int(input("second num is : "))
+
+print(f"(first num) + (second num) = {num1 + num2}")
+print("")

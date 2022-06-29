024e1c2c6e781ddb4f0c39ab7acdfb0d12c722f2
captainazm <kouki.eastriv9@gmail.com>
2022-06-29 17:05:25 +0900

    / addition

--- sample.py
+++ sample.py
@@ -1,7 +1,8 @@
-num1 = int(input("first num is : "))
-num2 = int(input("second num is : "))
+num1 = float(input("first num is : "))
+num2 = float(input("second num is : "))
 
 print(f"(first num) + (second num) = {num1 + num2}")
 print(f"(first num) - (second num) = {num1 - num2}")
 print(f"(first num) x (second num) = {num1 * num2}")
+print(f"(first num) / (second num) = {num1 / num2}")
 
